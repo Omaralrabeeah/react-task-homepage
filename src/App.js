2212957942b7fc411c19b1e31cd24f2684815c2d
Omaralrabeeah <omar.alrabeeah@gmail.com>
2023-10-01 21:10:19 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Prodacuts from "./Prodacuts";
 function App() {
+  const productlist = Prodacuts.map((product) => {
+    return (
+      <div>
+        <img src={product.image} alt="" className="product-image"></img>
+        <h4>{product.name}</h4>
+        <h4>{product.price}</h4>
+      </div>
+    );
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cookies and beyond</h1>
+      <h4>You'll never leave the cafe</h4>
+      <img
+        className="small"
+        alt=""
+        src="https://blog.common.com/blog/wp-content/uploads/2020/06/batchcookieco_90996522_573003480089527_373127690297320544_n.jpg"
+      />
+      <div className="omar">{productlist}</div>
     </div>
   );
 }
